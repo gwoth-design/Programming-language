944cab4239d140facf9ab591b5216fa0942f1582
gwoth-design <benjaminaherman@gmail.com>
2023-12-03 18:35:46 +0000

    new file:   .vscode/settings.json 	modified:   lexer.ts 	new file:   test.lum

--- lexer.ts
+++ lexer.ts
@@ -6,11 +6,94 @@ export enum TokenType{
     OpenParen, 
     CloseParen,
     BinaryOperator,
-
     Let,
+}
+
+const KEYWORDS: Record<string, TokenType> = {
+    "let": TokenType.Let,
 }
 
 export interface Token{
     value: string,
     type: TokenType,
+}
+
+function token (value = "", type: TokenType): Token{
+    return {value, type};
+}
+
+function isalpha(src: string){
+    return src.toUpperCase() != src.toLowerCase();
+}
+
+function isskippable (str: string){
+    return str == ' ' || str == '\n' || str == '\t'
+}
+
+function isint(str: string){
+    const c = str.charCodeAt(0);
+    const bounds = ['0'.charCodeAt(0), '9'.charCodeAt(0)];
+    return (c >= bounds[0] && c <= bounds[1])
+}
+
+export function tokenize (sourceCode: string): Token[]{
+    const tokens = new Array<Token>();
+    const src = sourceCode.split("");
+
+    //continue reading and making tokens until the end of the file
+    while (src.length > 0){
+        if(src[0] == "("){
+            tokens.push(token(src.shift(), TokenType.OpenParen));
+        }
+        else if(src[0] == ")"){
+            tokens.push(token(src.shift(), TokenType.CloseParen));
+        }
+        else if(src[0] == "+" || src[0] == "-" || src[0] == "*" || src[0] == "/"){
+            tokens.push(token(src.shift(), TokenType.BinaryOperator));
+        }
+        else if(src[0] == "="){
+            tokens.push(token(src.shift(), TokenType.Equals));
+        }
+        else{
+            //this handles all multicharacter tokens
+
+            //create the number token
+            if(isint(src[0])){
+                let num = ""
+                while(src.length > 0 && isint(src[0])){
+                    num += src.shift();
+                }
+                tokens.push(token(num, TokenType.Number));
+            }
+            else if(isalpha(src[0])){
+                let ident = ""
+                while(src.length > 0 && isalpha(src[0])){
+                    ident += src.shift();
+                }
+
+                //check if is a reserved keyword
+                const reserved = KEYWORDS[ident]
+                if(reserved == undefined){
+                    tokens.push(token(ident, TokenType.Identifier));
+                }
+                else{
+                    tokens.push(token(ident, reserved));
+                }
+            }
+            else if(isskippable(src[0])){
+                src.shift(); // skip the character
+            }
+            else{
+                console.log("Unrecognised character found in source: ", src[0]);
+                Deno.exit(1);
+            }
+        }
+    }
+
+    return tokens;
+}
+
+const source = await Deno.readTextFile("./test.lum");
+for(const token of tokenize(source)){
+    console.log(token);
 }
